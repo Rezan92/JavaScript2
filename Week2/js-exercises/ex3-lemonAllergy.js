3ed51cc3998651594dd74f792f15a789c7f9f283
Rezan92 <rezanibrahim8@gmail.com>
2020-08-11 20:39:25 +0200

    Finished ex3-lemonAllergy

--- Week2/js-exercises/ex3-lemonAllergy.js
+++ Week2/js-exercises/ex3-lemonAllergy.js
@@ -14,6 +14,8 @@
 
 function takeOutLemons(basket) {
   // your code goes in here. The output is a string 
+   const fruits = basket.filter(fruit => fruit != "Lemon");
+   return `My mom bought me a fruit basket, containing [${fruits}]`;
 }
 
 const fruitBasket = ['Apple', 'Lemon', 'Grapefruit', 'Lemon', 'Banana', 'Watermelon', 'Lemon'];
