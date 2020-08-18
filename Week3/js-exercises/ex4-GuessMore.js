e91b1a3c22610c816d4325aebc8fbbdf4b2f512d
Rezan92 <rezanibrahim8@gmail.com>
2020-08-18 05:53:37 +0200

    Finished ex3-GuessMore

--- Week3/js-exercises/ex4-GuessMore.js
+++ Week3/js-exercises/ex4-GuessMore.js
@@ -27,3 +27,8 @@ function f2(val) {
 }
 f2(y);
 console.log(y);
+
+/*
+1 - The first function will throw an error because we are reassigning a variable declared using the keyword const which is not allowed in JavaScript.
+2 - The second function will add 1 to the property x of the object y, in this case y is still the same object we are not reassigning it, we are adding 1 to its property which is allowed in JavaScript.
+*/
