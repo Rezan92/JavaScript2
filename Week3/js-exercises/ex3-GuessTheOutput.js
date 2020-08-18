0ed1be0eb5700c2f76f19780d7c8dab8d6581d81
Rezan92 <rezanibrahim8@gmail.com>
2020-08-18 05:40:21 +0200

    Finished ex3-GuessTheOutPut

--- Week3/js-exercises/ex3-GuessTheOutput.js
+++ Week3/js-exercises/ex3-GuessTheOutput.js
@@ -19,3 +19,10 @@ const x = (function () {
 })();
 
 x();
+
+/*
+1 - When we declared the variable x we asigned it to a function and we called it right away (x=(function)(call function)).
+2 - The function contain a closure, the closure give the inner function reference to all the variables in the lexical environment(the parent function in this case),
+    in the parent function we reassign the variable a to 12 the inner function sees that and use it in the alert.
+3 - The line 14 will return the inner function (closure), and at the line 21 we call the returned function ( the inner function) which will alert 12.
+*/
